9d49d54c487b64a11bcfadbb537c5da35a3f5645
duke <sendebopriyo10@gmail.com>
2025-04-19 01:33:07 +0530

    auth backend

---
+++ backend/routes/user.route.js
@@ -0,0 +1,30 @@
+import express from "express"
+import { 
+    forgotPassword, 
+    getUser, 
+    login, 
+    logout, 
+    register, 
+    resetPassword, 
+    verifyOtp,
+    getAllNGOs ,
+    getUserDetails,
+} from "../controllers/user.controller.js";
+import { isAuthenticated } from "../middlewares/auth.middleware.js";
+
+const router = express.Router();
+
+// Auth routes
+router.post('/register', register);
+router.post("/verify-otp", verifyOtp);
+router.post("/login", login);
+router.get("/logout", isAuthenticated, logout);
+router.get("/profile", isAuthenticated, getUser);
+router.get("/:id",getUserDetails);
+router.post("/password/forgot", forgotPassword);
+router.put("/password/reset/:token", resetPassword);
+
+// NGO routes
+router.get('/ngos', isAuthenticated, getAllNGOs);
+
+export default router;
