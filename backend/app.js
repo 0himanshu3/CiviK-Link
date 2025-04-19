de29dc8af91d491d9cfc01fbc4439a7536c0e50b
Satyam Jha <satyamjhadar@gmail.com>
2025-04-19 16:03:32 +0530

    modified some frontend

--- backend/app.js
+++ backend/app.js
@@ -29,7 +29,7 @@ app.use(express.urlencoded({extended: true}));
 
 // Add this after creating the app but before any routes
 app.use((req, res, next) => {
-  console.log('Incoming request:', req.method, req.url);
+  // console.log('Incoming request:', req.method, req.url);
   next();
 });
 
